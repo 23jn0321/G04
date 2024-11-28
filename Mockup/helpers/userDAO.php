b367bb9419a990eb6c5a61cc790300288fa7e55e
古川日々輝 <23jn0321@jec.ac.jp>
2024-11-28 16:39:53 +0900

    edit完成寸前

--- Mockup/helpers/userDAO.php
+++ Mockup/helpers/userDAO.php
@@ -49,8 +49,8 @@ class userDAO
         $stmt = $dbh->prepare($sql);
 
 
-        $stmt->bindValue(":profilecomment", $user->profilecomment, PDO::PARAM_STR);
-        $stmt->bindValue(":username", $user->username, PDO::PARAM_STR);
+        $stmt->bindValue(":profilecomment", $user->ProfileComment, PDO::PARAM_STR);
+        $stmt->bindValue(":username", $user->UserName, PDO::PARAM_STR);
 
         $stmt->execute();
     }
