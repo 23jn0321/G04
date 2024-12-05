6b3adb58f9a265e7c84e5e03ab7d4b2bee63120e
會澤仁 <23jn0321@jec.ac.jp>
2024-12-05 16:41:45 +0900

    genreDao作成

--- Mockup/helpers/GenreDAO.php
+++ Mockup/helpers/GenreDAO.php
@@ -2,14 +2,14 @@
     require_once 'DAO.php';
 
 class Genre{
-    public int $mainGenreID;
-    public string $mainGenreName;
+    public int $mainGenreID;        //メインジャンルID
+    public string $mainGenreName;   //メインジャンルネーム
 }
 class SubGenre{
-    public int $subGenreID;
-    public int $mainGenreID;
-    public string $subGenreName;
-    public int $deleteFlag;
+    public int $subGenreID;         //サブジャンルID
+    public int $mainGenreID;        //メインジャンルID
+    public string $subGenreName;    //サブジャンルネーム
+    public bool $deleteFlag;         //サブジャンルのdeleteflag
 }
 
 class GenreDAO{
@@ -20,7 +20,39 @@ class GenreDAO{
 
         $stmt = $dbh->query($sql);
 
+        $genres = [];
         
+        // 取得したデータをループして Genre オブジェクトに格納
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $genre = new Genre();
+            $genre->mainGenreID = $row['mainGenreID'];
+            $genre->mainGenreName = $row['mainGenreName'];
+            $genres[] = $genre;
+        }
+
+        return $genres;
+        
+    }
+}
+class subGenreDAO{
+    public function get_subGenre(){
+        $dbh=DAO::get_db_connect();
+
+        $sql="SELECT * FROM SubGenre";
+        
+        $stmt=$dbh->query($sql);
+
+        $subGenres=[];
+
+        while($row=$stmt->fetch(PDO::FETCH_ASSOC)){
+            $subGenre=new subGenre();
+            $subGenre->subGenreID=$row['SubGeneID'];
+            $subGenre->mainGenreID=$row['MainGenre'];
+            $subGenre->subGenreName=$row['SubGenreName'];
+            $subGenre->deleteFlag=$row['DeleteFlag'];
+            $subGenres[]=$subGenre;
+        }
+        return $subGenres;
     }
 }
 ?>
