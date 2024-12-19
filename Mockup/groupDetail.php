2566a001734611d6fb6948ae0188ce0e640cc622
大橋雪乃成 <23jn0321@jec.ac.jp>
2024-12-19 17:03:47 +0900

    Css途中

--- Mockup/groupDetail.php
+++ Mockup/groupDetail.php
@@ -1,47 +1,47 @@
 <?php
-    require_once 'helpers/GroupDetailDAO.php';
-    require_once 'helpers/userDAO.php';
-    require_once 'helpers/GroupDAO.php';
-   
-    include "header.php"; 
-  
-    $loggedInUser = null;
+require_once 'helpers/GroupDetailDAO.php';
+require_once 'helpers/userDAO.php';
+require_once 'helpers/GroupDAO.php';
 
-if (isset($_SESSION['userInfo']) ) {
-    $userInfo = $_SESSION['userInfo'];
+include "header.php";
 
-    $loggedInUser = $_SESSION['userInfo'];
-}else{
+$loggedInUser = null;
+
+if (isset($_SESSION['userInfo'])) {
+  $userInfo = $_SESSION['userInfo'];
+
+  $loggedInUser = $_SESSION['userInfo'];
+} else {
   header("Location: login.php");
   exit;
 }
-    $groupDAO = new GroupDAO();
-   
-    $groupInfo = $groupDAO->getGroup($loggedInUser->UserID);
+$groupDAO = new GroupDAO();
 
-    //セッションの開始
-    if(session_status() === PHP_SESSION_NONE){
-      session_start();
+$groupInfo = $groupDAO->getGroup($loggedInUser->UserID);
+
+//セッションの開始
+if (session_status() === PHP_SESSION_NONE) {
+  session_start();
 }
-  if(isset($_GET['GroupID'])){
-    //リクエストパラメータのgroupIDを取得する
-    $groupID = $_GET['GroupID'];
+if (isset($_GET['GroupID'])) {
+  //リクエストパラメータのgroupIDを取得する
+  $groupID = $_GET['GroupID'];
 }
-  
-  $groupdetail = new GroupDetailDAO();
-  $group = $groupdetail->get_GroupDetail1($groupID);
 
-  $groupdetail2 = new GroupDetailDAO();
-  $group_list = $groupdetail2->get_groupDetail2($groupID);
+$groupdetail = new GroupDetailDAO();
+$group = $groupdetail->get_GroupDetail1($groupID);
 
-  if($_SERVER["REQUEST_METHOD"] === "POST"){
-    $groupdetail3 = new GroupDetailDAO();
-    $groupdetail3 -> insert($userInfo->UserID,$groupID);
-    var_dump($userInfo);
+$groupdetail2 = new GroupDetailDAO();
+$group_list = $groupdetail2->get_groupDetail2($groupID);
 
-    header('Location: message.php?GroupID='. urlencode($groupID));
-    exit;
-  }
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+  $groupdetail3 = new GroupDetailDAO();
+  $groupdetail3->insert($userInfo->UserID, $groupID);
+  var_dump($userInfo);
+
+  header('Location: message.php?GroupID=' . urlencode($groupID));
+  exit;
+}
 
 
 ?>
@@ -61,11 +61,14 @@ if (isset($_SESSION['userInfo']) ) {
 
 </header>
 <form id="joinButton" action="" method="POST">
-<div>
-  <p id="group">所属グループ一覧</p>
-  <input type="hidden" name="deleteGroup" value="1">
-  <input type="submit" value="参加" id="join">
-</div>
+  <div>
+
+    <body>
+      <p id="group">所属グループ一覧</p>
+      <input type="hidden" name="deleteGroup" value="1">
+      <input type="submit" value="参加" id="join">
+    </body>
+  </div>
 </form>
 <a href="genreSelect.html"><input type="button" value="ジャンル選択に戻る" id="back"></a>
 <script src="./jquery-3.6.0.min.js"></script>
@@ -73,62 +76,62 @@ if (isset($_SESSION['userInfo']) ) {
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 <script>
-$(document).ready(function() {
+  $(document).ready(function() {
     // フォームの送信イベントをカスタマイズ
     $('#joinButton').on('submit', function(e) {
-        e.preventDefault(); // デフォルトの送信処理を防ぐ
+      e.preventDefault(); // デフォルトの送信処理を防ぐ
 
-        const form = this; // フォーム要素を参照
+      const form = this; // フォーム要素を参照
 
-        // SweetAlert2を使って確認ダイアログを表示
-        Swal.fire({
-            title: '<?= $group[0]['GroupName'] ?>に参加しますか？', // ダイアログのタイトル
-            icon: 'question', // アイコン（質問マーク）
-            showCancelButton: true, // キャンセルボタンを表示
-            confirmButtonText: '確定', // 確定ボタンのテキスト
-            cancelButtonText: 'キャンセル', // キャンセルボタンのテキスト
-            reverseButtons: true
-        }).then((result) => {
-            if (result.isConfirmed) {
-                // 確定ボタンが押された場合、フォームを送信
-                form.submit();
-            }
-        });
+      // SweetAlert2を使って確認ダイアログを表示
+      Swal.fire({
+        title: '<?= $group[0]['GroupName'] ?>に参加しますか？', // ダイアログのタイトル
+        icon: 'question', // アイコン（質問マーク）
+        showCancelButton: true, // キャンセルボタンを表示
+        confirmButtonText: '確定', // 確定ボタンのテキスト
+        cancelButtonText: 'キャンセル', // キャンセルボタンのテキスト
+        reverseButtons: true
+      }).then((result) => {
+        if (result.isConfirmed) {
+          // 確定ボタンが押された場合、フォームを送信
+          form.submit();
+        }
+      });
     });
-});
-
-    </script>
+  });
+</script>
 
 
 
 
 <nav class="group">
-    <ul>
+  <ul>
     <?php foreach ($groupInfo as $var): ?>
       <li>
         <a href="message.php?GroupID=<?= urlencode($var->GroupID) ?>">
-          <?= $var->GroupName?>（<?= $var->MemberInfo?>）<br>最終更新日：<?=$var->LastUpdated?><br>ジャンル：<?= $var->Genre ?>
+          <?= $var->GroupName ?>（<?= $var->MemberInfo ?>）<br>最終更新日：<?= $var->LastUpdated ?><br>ジャンル：<?= $var->Genre ?>
         </a>
-       
-        <?php if($loggedInUser->UserID == $var->GroupAdminID) : ?>
-         <input type="button" onclick="location.href='groupEdit.php?GroupID=<?= urlencode($var->GroupID)?>'" id="groupEditR" value="グループ編集">
-          <?php endif; ?>
+
+        <?php if ($loggedInUser->UserID == $var->GroupAdminID) : ?>
+          <input type="button" onclick="location.href='groupEdit.php?GroupID=<?= urlencode($var->GroupID) ?>'" id="groupEditR" value="グループ編集">
+        <?php endif; ?>
       </li>
+    <?php endforeach; ?>
+  </ul>
+  </a>
+
+
+
+  <p id="groupName">グループ名：<?= $group[0]['GroupName'] ?>(<?= $group[0]['MemberInfo'] ?>)</p>
+
+  <p id="groupGenre">グループのジャンル：<?= $group[0]['Genre'] ?> </p>
+  <div class="aaa">
+    <ul1>
+      <?php foreach ($group_list as $var) : ?>
+        <li1><?= $var->UserName ?><br><?= $var->GakkaName ?></li1>
       <?php endforeach; ?>
-    </ul>
-</a>
-
-  
-
-<p id="groupName">グループ名：<?= $group[0]['GroupName'] ?> (<?= $group[0]['MemberInfo'] ?>)</p>
-<p id="groupGenre">グループのジャンル：<?= $group[0]['Genre'] ?> </p>
-<div class="aaa">
-<ul1>
-<?php foreach($group_list as $var) : ?>
-  <li1><?= $var->UserName ?><br><?= $var->GakkaName ?></li1>
-<?php endforeach; ?>
-</ul1>
-</div>
+    </ul1>
+  </div>
 
 
-<p id=groupEdit>グループ詳細</p><input type="text" id="txtGE" value=<?= $group[0]['GroupDetail'] ?> readonly>
+  <p id=groupEdit>グループ詳細</p><input type="text" id="txtGE" value=<?= $group[0]['GroupDetail'] ?> readonly>
